ab00c5a9cb1d79a6310fd4c191e33ecf0a1f1fa0
srilatha2012 <srilathp2012@gmail.com>
2026-05-25 14:16:50 -0400

    Initialize Express server with test route

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 //DEPENDENCIES
-const express = require("require");
+//Load environment variables
+require("dotenv").config();
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+//Import dependencies
+const express = require("express");
+
+//Create Express application
+const app = express();
+
+//Define PORT number
+const PORT = process.env.PORT || 3000;
 
 //MIDDLEWARE
 //ROUTES
+app.get("/test", (req,res) =>{
+    res.send("testing TaskMaster API..");
+});
+
 //PORT / LISTEN
+app.listen(PORT, () =>{
+    console.log(`Server is listening on PORT ${PORT}`);
+})
