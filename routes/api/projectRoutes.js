eec6aecf78cf338cce84df9d4039e9431ff0bfe0
srilatha2012 <srilathp2012@gmail.com>
2026-05-26 19:13:54 -0400

    Added PUT request to update the project by project id, owned by the user

--- routes/api/projectRoutes.js
+++ routes/api/projectRoutes.js
@@ -35,26 +35,40 @@ projectRoutes.get("/projects", async (req, res) => {
         const projects = await Project.find({ user: req.user._id });
         res.json(projects);
     } catch (error) {
-       res.status(500).json({error});
+        res.status(500).json({ error });
     }
 });
 
 //Get a single project by ID, owned by the user which currently logged-in
 projectRoutes.get("/projects/:id", async (req, res) => {
- try {
-      const projectId = req.params.id;
-      const project = await Project.findById(projectId);
-      res.json(project);
- } catch(error) {
-    res.status(500).json({error});
- }
+    try {
+        const projectId = req.params.id;
+        const project = await Project.findById(projectId);
+        res.json(project);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
 
 });
-
-projectRoutes.put("/projects/:id", (req, res) => {
-
+//update single project by Id, owned by the user
+projectRoutes.put("/projects/:id", async (req, res) => {
+    try {
+        const projectId = req.params.id;
+        const project = await Project.findById(projectId);
+        if(!project) {
+            return res.status(404).json({message: `No Project found with this project id: ${projectId}`});
+        }
+        if(project.user.toString() !== req.user._id.toString()) {
+            return res.status(403).json({message: "User is not authorized to update this project."});
+        }
+        const updatedProject = await Project.findByIdAndUpdate(projectId, req.body, {new: true,});
+        res.json(updatedProject);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
 });
 
+//delete a single project by ID ,owned by the user
 projectRoutes.delete("/projects/:id", (req, res) => {
 
 });
