e2b8826d3a03a7afe007c22b1db2457b4556817b
srilatha2012 <srilathp2012@gmail.com>
2026-05-27 14:05:31 -0400

    Added Get request to get all tasks based on the projectId

--- routes/api/taskRoutes.js
+++ routes/api/taskRoutes.js
@@ -7,8 +7,6 @@ taskRoutes.use(authMiddleware)
 //Create task - POST
 taskRoutes.post("/api/projects/:projectId/tasks", async (req, res) => {
   try {
-    console.log("parama", req.params.projectId);
-    console.log("userid", req.user._id);
     const project = await Project.findOne({
       _id: req.params.projectId,
       user: req.user._id
@@ -27,10 +25,22 @@ taskRoutes.post("/api/projects/:projectId/tasks", async (req, res) => {
   }
 });
 
-
-taskRoutes.put("/:taskId", async (req, res) => {
-
-})
+//Get all tasks based on project id
+taskRoutes.get("/api/projects/:projectId/tasks", async (req, res) => {
+  try {
+    const project = await Project.findOne({
+      _id: req.params.projectId,
+      user: req.user._id
+    });
+    if(!project) {
+      return res.status(403).json({message: "Not authorized for this project"});
+    }
+    const tasks = await Task.find({project: req.params.projectId});
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 
 module.exports = taskRoutes;
